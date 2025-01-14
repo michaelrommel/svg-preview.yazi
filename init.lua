d099d1b39344a549b87155e39841c6bdac3415e3
Michael Rommel <rommel@layer-7.net>
2025-01-14 08:11:00 +0100

    fix: first working version

--- init.lua
+++ init.lua
@@ -1,27 +1,61 @@
 local M = {}
 
-function M:peek()
-	local start, cache = os.clock(), ya.file_cache(self)
-	if not cache or self:preload() ~= 1 then
-		ya.sleep(math.max(0, PREVIEW.image_delay / 1000 + start - os.clock()))
-		ya.manager_emit("peek", { 0, only_if = self.file.url })
+function M:peek(job)
+	local cache = ya.file_cache(job)
+	if not cache then
 		return
 	end
 
-	ya.image_show(cache, self.area)
-	ya.preview_widgets(self, {})
+	if not self:preload(job) then
+		return
+	end
+
+	-- local t = io.open(tostring(cache), "r")
+	-- if t == nil then
+	-- 	return 0
+	-- end
+	--
+	-- local thumb = Url(t:read())
+	-- t:close()
+
+	ya.image_show(cache, job.area)
+	ya.preview_widgets(job, {})
 end
 
 function M:seek() end
 
-function M:preload()
-	local cache = ya.file_cache(self)
+M.dump = function(t)
+	local conv = {
+		["nil"] = function() return "nil" end,
+		["number"] = function(n) return tostring(n) end,
+		["string"] = function(s) return '"' .. s .. '"' end,
+		["boolean"] = function(b) return tostring(b) end,
+		["function"] = function(_) return "function()" end,
+	}
+	if type(t) == "table" then
+		local s = "{"
+		for k, v in pairs(t) do
+			if type(v) == "table" then
+				s = s .. (s == "{" and " " or ", ") .. (k .. " = " .. M.dump(v))
+			else
+				s = s .. (s == "{" and " " or ", ") .. k .. " = " .. conv[type(v)](v)
+			end
+		end
+		return s .. " }"
+	else
+		return conv[type(t)](t)
+	end
+end
+
+
+function M:preload(job)
+	local cache = ya.file_cache(job)
 	if not cache or fs.cha(cache) then
 		return 1
 	end
 
 	local output = Command("inkscape")
-		:args({ "-W", "-H", tostring(self.file.url) })
+		:args({ "-W", "-H", tostring(job.file.url) })
 		:stdout(Command.PIPED)
 		:stderr(Command.PIPED)
 		:output()
@@ -34,25 +68,34 @@ function M:preload()
 
 	local height
 	local width
-	_, _, width, height = string.find(output, "(%s)\n(%s)")
+	_, _, width, height = string.find(output.stdout, "(%d+).*\n(%d+).*\n")
 
 	local max_width = (width / height) >= (PREVIEW.max_width / PREVIEW.max_height)
 
-	local child, code = Command("inkscape"):args({
+	local args = {
 		"--export-type=png",
 		max_width and "--export-width" or "--export-height",
 		max_width and tostring(PREVIEW.max_width) or tostring(PREVIEW.max_height),
-		tostring(self.file.url),
-		"-o", tostring(cache)
-	}):spawn()
+		tostring(job.file.url),
+		"-o", "-"
+	}
+	local child, code = Command("inkscape"):args(args):stdout(Command.PIPED):spawn()
 
 	if not child then
 		ya.err("spawn `inkscape` command returns " .. tostring(code))
 		return 0
 	end
 
-	local status = child:wait()
-	return status and status.success and 1 or 2
+	local child_output, err = child:wait_with_output()
+	if err then
+		ya.err("inkscape returned an error" .. tostring(err))
+		return 0
+	end
+
+	local thumb = child_output.stdout
+	ya.dbg("length: " .. string.len(thumb))
+	return fs.write(cache, thumb) and 1 or 2
+	-- return status and status.success and 1 or 2
 end
 
 return M
